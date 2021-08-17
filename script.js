9483d2b7e638e72195d9db89cf47a099590e7285
Reuben Vinod <78326515+lucky-quinn@users.noreply.github.com>
2021-08-17 23:22:20 +0530

    Created the webpage

---
+++ script.js
@@ -0,0 +1,33 @@
+function make1()
+{
+    document.body.style.backgroundColor = "#ea4335";
+
+}
+function make2()
+{
+    document.body.style.backgroundColor = "#4285f4";
+}
+function make3()
+{
+    document.body.style.backgroundColor = "#FF6F00";
+}
+function make4()
+{
+    document.body.style.backgroundColor = "#34a853";
+}
+function make5()
+{
+    document.body.style.backgroundColor = "#fbbc04";
+}
+function make6()
+{
+    document.body.style.backgroundColor = "#01579B";
+}
+function make7()
+{
+    document.body.style.backgroundColor = "#F50057";
+}
+function make8()
+{
+    document.body.style.backgroundColor = "#E91E63";
+}
